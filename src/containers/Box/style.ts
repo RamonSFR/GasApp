2bd0b9646e46b7574fe6c6dfbacea32f899e73ed
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-03-25 15:29:56 -0300

    box container

---
+++ src/containers/Box/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import variables from '../../styles/variables'
+
+export const Div = styled.div`
+  background-color: ${variables.white};
+  padding: 16px 8px;
+  border-radius: 7px;
+  box-shadow: 7px 7px 7px rgba(0.1,0.1,0.1,0.);
+`
